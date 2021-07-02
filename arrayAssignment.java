fcb8e4473492cc865428937267667b224e4a0851
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-02 15:48:24 -0700

    array stuff

---
+++ arrayAssignment.java
@@ -0,0 +1,8 @@
+public class main{
+    
+public static void main(String[] args) {
+    int array = [1,1,0,1,1,1];
+    findMaxConsecutiveOnes(array);
+}
+
+}
---
+++ findMaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        // Hint: Initialise and declare a variable here to 
+        // keep track of how many 1's you've seen in a row.
+        int count = 0;
+        int [] size = new int[5];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Do something with element nums[i].
+            if(nums[i] == 1){
+                count++;
+            }
+            else if(nums[i] == 0){
+                size[j] = count;
+                j++;
+            }
+        }
+        int max = 0;
+        for(int temp : size){
+            if(temp >= max){
+                max = temp;
+            }
+        }
+        return max;
+    }
