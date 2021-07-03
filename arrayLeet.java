3609719d7962a4550782511cde22baaa0b1aea8a
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-02 17:00:33 -0700

    now correct

--- arrayLeet.java
+++ arrayLeet.java
@@ -6,22 +6,32 @@ public class arrayLeet{
         // keep track of how many 1's you've seen in a row.
         int count = 0;
         int max = 0;
+        int count1 = 0;
+        int count0 = 0;
         for (int k : nums) {
             // Do something with element nums[i].
+            if(k == 1){
+                count1++; 
+            }
+            else if (k == 0){
+                count0++;
+            }
             if(k == 1){
                 count++;
             }
             else{
                 if(count >= max){
                     max = count;
-                    count = 0;
                 }
+                count = 0;
             }
         }
         if(count >= max){
             max = count;
         }
         System.out.println(max);
+        System.out.println("1: " + count1);
+        System.out.println("0: " + count0);
         return max;
     }
 }
