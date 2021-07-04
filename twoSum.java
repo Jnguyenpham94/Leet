b46132852f7f898d33a65840514ce61f06359c31
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-03 19:06:12 -0700

    two sums

---
+++ twoSum.java
@@ -0,0 +1,15 @@
+public class twoSum{
+
+    public int[] sumTwo(int[] nums, int target) {
+        int sum = 0;
+        int index1 = 0;
+        int index2 = 0;
+        if(sum != target){
+            for(int i : nums){
+                
+            }
+        }
+        return nums;
+        
+    }
+}
