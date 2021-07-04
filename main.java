55341361755b8d429b3a2607b47892e582088181
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-03 21:07:58 -0700

    added print results

--- main.java
+++ main.java
@@ -3,10 +3,10 @@ public class main{
 public static void main(String[] args) {
     int [] array = {1,1,1,1,0,1,1,1,1,1,};
     arrayLeet test = new arrayLeet();
-    test.findMaxConsecutiveOnes(array);
+    //test.findMaxConsecutiveOnes(array);
     twoSum test2 = new twoSum();
     int [] nums = {2,7,11,15};
-    int target = 6;
+    int target = 9;
     System.out.println("SumTwo: ");
     test2.sumTwo(nums, target);
 }
--- twoSum.java
+++ twoSum.java
@@ -1,17 +1,20 @@
 public class twoSum {
 
-    //{3,2,4}
+    // {3,2,4}
     public int[] sumTwo(int[] nums, int target) {
-            for(int i = 0; i < nums.length;i++){
-                for(int k = i+1; k< nums.length;k++){
-                    if(nums[i] + nums[k] == target){
-                        return new int [] {i,k};
-                    }
-                    }
+        int[] index = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                if (nums[i] + nums[k] == target) {
+                    index[0] = i;
+                    index[1] = k;
+                    System.out.println(index[0]);
+                    System.out.println(index[1]);
+                    return new int[] { i, k };
+                }
             }
-        //System.out.println(index[0]);
-        //System.out.println(index[1]);
-        return new int [] {};
-}
+        }
+        return new int[] {};
+    }
 
 }
