c8ffba208f90841febddb25afc47fd1b750d785f
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-03 19:50:58 -0700

    almost there

--- twoSum.java
+++ twoSum.java
@@ -1,15 +1,30 @@
-public class twoSum{
+public class twoSum {
 
+    int [] index = {0,0};
+    
     public int[] sumTwo(int[] nums, int target) {
         int sum = 0;
-        int index1 = 0;
-        int index2 = 0;
+
         if(sum != target){
-            for(int i : nums){
-                
+            for(int i = 0; i < nums.length - 1;i++){
+                for(int k = 1; k< nums.length - 1;k++){
+                    if(sum != target){
+                        index[0] = i;
+                        index[1] = k; 
+                        sum = nums[i] + nums[k];
+                    }
+                }
             }
         }
-        return nums;
-        
+        System.out.println(index.toString());
+        return index;
+}
+
+    /*overrides toString for the above 
+    to show the index values in String format [#,#]
+    */
+    @Override
+    public String toString() {
+        return "[" + index[0] + ", " + index[1] + "]";
     }
 }
