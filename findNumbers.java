f88992c9507648c5c9d5d96e5859e86948459dcd
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-04 19:23:53 -0700

    findNumbers

---
+++ findNumbers.java
@@ -0,0 +1,13 @@
+public class findNumbers {
+    
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i : nums){
+            if(nums[i]/10 == ){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
--- main.java
+++ main.java
@@ -4,11 +4,16 @@ public static void main(String[] args) {
     int [] array = {1,1,1,1,0,1,1,1,1,1,};
     arrayLeet test = new arrayLeet();
     //test.findMaxConsecutiveOnes(array);
+    
     twoSum test2 = new twoSum();
     int [] nums = {2,7,11,15};
     int target = 9;
     System.out.println("SumTwo: ");
-    test2.sumTwo(nums, target);
+    //test2.sumTwo(nums, target);
+
+    findNumbers test3 = new findNumbers();
+    int [] array2 = {12,345,2,6,7896};
+    test3.findNumbers(array2);
 }
 
 }
