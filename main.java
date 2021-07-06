954e058a3670dd9c3bf5e57b9b21d01e5abc207e
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-05 23:29:56 -0700

    basic menu works

--- main.java
+++ main.java
@@ -1,25 +1,9 @@
 public class main{
     
 public static void main(String[] args) {
-    int [] array = {1,1,1,1,0,1,1,1,1,1,};
-    arrayLeet test = new arrayLeet();
-    //test.findMaxConsecutiveOnes(array);
-    
-    twoSum test2 = new twoSum();
-    int [] nums = {2,7,11,15};
-    int target = 9;
-    System.out.println("SumTwo: ");
-    //test2.sumTwo(nums, target);
 
-    System.out.println("find Even numbers: ");
-    findNumbers test3 = new findNumbers();
-    int [] array2 = {12,345,2,6,7896};
-    //test3.findEvenNumbers(array2);
-
-    sortedSquares test4 = new sortedSquares();
-    System.out.println("find sorted squares: ");
-    int [] array3 = {-4,-1,0,3,10};
-    //test4.sortedSquareArray(array3);
+    menu into = new menu();
+    into.mainMenu();
 }
 
 }
--- menu.java
+++ menu.java
@@ -1,4 +1,46 @@
+import java.util.Scanner;
+
 public class menu {
+
+    public void menuChoices(){
+        System.out.println("1. find Max Consecutive.");
+        System.out.println("2. Two Sum");
+        System.out.println("3. Find even numbers");
+        System.out.println("4. Find sorted squares");
+    }
     
-    
+    public void mainMenu(){
+        Scanner in = new Scanner(System.in);
+        menuChoices();
+        int choice = in.nextInt();
+        switch(choice){
+            case 1:
+            int [] array = {1,1,1,1,0,1,1,1,1,1,};
+            arrayLeet test = new arrayLeet();
+            test.findMaxConsecutiveOnes(array);
+            break;
+            case 2:
+            twoSum test2 = new twoSum();
+            int [] nums = {2,7,11,15};
+            int target = 9;
+            System.out.println("SumTwo: ");
+            test2.sumTwo(nums, target);
+            break;
+            case 3:
+            System.out.println("find Even numbers: ");
+            findNumbers test3 = new findNumbers();
+            int [] array2 = {12,345,2,6,7896};
+            test3.findEvenNumbers(array2);
+            break;
+            case 4:
+            sortedSquares test4 = new sortedSquares();
+            System.out.println("find sorted squares: ");
+            int [] array3 = {-4,-1,0,3,10};
+            test4.sortedSquareArray(array3);
+            break;
+            default:
+            in.close();
+        }
+
+    }
 }
