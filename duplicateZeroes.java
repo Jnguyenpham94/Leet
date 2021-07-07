3457d611faca220ab74bcff0d9e7bcfa15ca77d0
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-06 22:57:06 -0700

    working on System.arraycopy

--- duplicateZeroes.java
+++ duplicateZeroes.java
@@ -1,8 +1,13 @@
 public class duplicateZeroes {
     
+    //{1,0,2,3,0,4,5,0}
     public void duplicateZeros(int[] arr) {
+        int [] copy = arr.clone();
         for(int i = 0; i < arr.length;i++){
-            
+            if(arr[i] == 0){
+                arr[i+1] = 0;
+                System.arraycopy(src, srcPos, dest, destPos, length);
+            }
         }
     }
 }
