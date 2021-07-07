b390e3ac0ce9daada858da0ae73da406fc9b2104
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-06 23:09:04 -0700

    array out of bounds!

--- duplicateZeroes.java
+++ duplicateZeroes.java
@@ -1,13 +1,23 @@
 public class duplicateZeroes {
     
     //{1,0,2,3,0,4,5,0}
+    //EXPECTED:{1,0,0,2,3,0,0,4}
     public void duplicateZeros(int[] arr) {
         int [] copy = arr.clone();
         for(int i = 0; i < arr.length;i++){
             if(arr[i] == 0){
                 arr[i+1] = 0;
-                System.arraycopy(src, srcPos, dest, destPos, length);
+                if(i != arr.length){
+                    System.arraycopy(copy, copy[i+1], arr, arr[i+1], arr.length);
+                }
             }
         }
+        arrayPrint(arr);
     }
+
+    public void arrayPrint(int [] arr){
+        for (int i : arr) {
+            System.out.println(i);
+        }
+}
 }
