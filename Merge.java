bc8ecebfdfb40cc36d0fdf801ff2fe5268da5c7a
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-08 18:07:24 -0700

    start

---
+++ Merge.java
@@ -0,0 +1,15 @@
+public class Merge {
+    
+    /**
+     * 
+     * @param nums1 {1,2,3,0,0,0}
+     * @param m
+     * @param nums2 {2,5,6}
+     * @param n
+     * EXPECTED nums1: {1,2,2,3,5,6}
+     */
+    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, , );
+    }
+}
