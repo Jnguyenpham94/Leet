0f36bc3e91a8cf2d70bed3f9b663b1e1961176db
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-11 14:26:20 -0700

    comment

--- RemoveElement.java
+++ RemoveElement.java
@@ -3,10 +3,10 @@ public class RemoveElement {
      * 
      * @param nums array of elements
      * @param val
-     * @return
+     * @return k = number of values remaining in nums
      */
     public int removeElement(int[] nums, int val) {
-        
+        int k = nums.length;
         return 0;
     }
 
