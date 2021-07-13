5c23ccd4df792debefa088cfdb3cb42d13d3a99f
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-12 21:14:24 -0700

    comment

--- CheckDouble.java
+++ CheckDouble.java
@@ -1,5 +1,9 @@
 public class CheckDouble {
-    
+    /**
+     * 
+     * @param arr array of elements
+     * @return true if a double of a value exists false other
+     */
     public boolean checkIfExist(int[] arr) {
         return true;
     }
