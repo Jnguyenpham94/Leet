a673945d58aa68d9dbfad053bef5d093c5bfc31f
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-13 22:02:31 -0700

    works?

--- CheckDouble.java
+++ CheckDouble.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class CheckDouble {
     /**
      * 
-     * @param arr array of elements
+     * @param arr array of elements {10,2,5,3}
      * @return true if a double of a value exists false other
      */
     public boolean checkIfExist(int[] arr) {
@@ -11,9 +11,15 @@ public class CheckDouble {
             return false;
         }
         Arrays.sort(arr);
-        for(int i = 0; i < arr.length;i++){
-            
+        for(int i = 0; i < arr.length -1;i++){
+            int m = arr[i] * 2;
+            System.out.println(arr[i]);
+            for(int j = i+1; j < arr.length; j++){
+                if(m == arr[j]){
+                    return true;
+                }
+            }
         }
-        return true;
+        return false;
     }
 }
