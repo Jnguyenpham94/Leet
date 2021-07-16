f460d3b0a2242ebcc5d4dd854a0220ec5414234c
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-15 19:29:24 -0700

    getting closer but still off

--- CheckDouble.java
+++ CheckDouble.java
@@ -1,27 +1,21 @@
-import java.util.Arrays;
 
 public class CheckDouble {
     /**
      * 
-     * @param arr array of elements {10,2,5,3}
+     * @param arr array of elements {3,1,7,11}
      * @return true if a double of a value exists false other
      */
     public boolean checkIfExist(int[] arr) {
-        //TODO: supposed to be false got true. -->{-950 -933 -918 -909 -892 -880 -857 -852 -815 -814 -803 -797 -741 -738 -691 -637 -592 -584 -488 -485 -453 -448 -360 -334 -242 -191 -132 -124 -93 -87 -40 26 50 74 88 98 102 250 304 356 360 449 457 461 476 508 522 540 563 639 665 745 750 802 806 833 840 868 881 883 991 999 -950}
-        if(arr == null || arr.length == 0){
+        // TODO: {1 3 7 11} should be true
+        if (arr == null || arr.length == 0) {
             return false;
         }
-        Arrays.sort(arr);//{-950 -933 -918 -909 -892 -880 -857 -852 -815 -814 -803 -797 -741 -738 -691 -637 -592 -584 -488 ***-485*** -453 -448 -360 -334 **-242** -191 -132 -124 -93 -87 -40 26 50 74 88 98 102 250 304 356 360 449 457 461 476 508 522 540 563 639 665 745 750 802 806 833 840 868 881 883 991 999}
-        for(int i = 0; i < arr.length -1;i++){
-            int m = arr[i] * 2;
-            int n = arr[i]/2;
-            for(int k = i+1; k < arr.length; k++){
-                if(arr[i] % 2 == 0){
-                    if(m == arr[k] || n == arr[k]){
-                        System.out.println("TRUE");
-                        System.out.println(i);
-                        return true;
-                    }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]*2 || arr[i] == arr[j]/2) {
+                    System.out.println("TRUE");
+                    System.out.println(i);
+                    return true;
                 }
             }
         }
