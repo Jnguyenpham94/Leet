bc7f78dd94a2a0e0169a7c1abb4b39141ce4e569
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-18 23:06:25 -0700

    javadoc

--- ReplaceElement.java
+++ ReplaceElement.java
@@ -1,5 +1,12 @@
 public class ReplaceElement {
     
+    /**
+     * 
+     * @param arr array of elements
+     * @return Given an array arr, replace every element in 
+     * that array with the greatest element among the elements to its right, 
+     * and replace the last element with -1.
+     */
     public int[] replaceElements(int[] arr) {
         return arr;
         
