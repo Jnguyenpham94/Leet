4bbdb63f1bde37840ec9a4e9ae00285aaf63f1b6
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-18 23:37:06 -0700

    really close

--- ReplaceElement.java
+++ ReplaceElement.java
@@ -6,6 +6,8 @@ public class ReplaceElement {
      * @return Given an array arr, replace every element in 
      * that array with the greatest element among the elements to its right, 
      * and replace the last element with -1.
+     * array  = {17,18,5,4,6,1}
+     * EXPECTED {18,6,6,6,1,-1}
      */
     public int[] replaceElements(int[] arr) {
         if (arr == null || arr.length == 0) {
@@ -18,9 +20,21 @@ public class ReplaceElement {
         int greatest = 0;
         for (int i = 0; i < arr.length; i++) {
             int current = arr[i];
+            greatest = current;
             for(int j = i + 1; j < arr.length; j++){
-                
+                if(arr[j] > current){
+                    greatest = arr[j];
+                }
             }
+            arr[i] = greatest;
+        }
+        //for the end arr value == -1
+        if(arr[arr.length-1] != -1){
+            arr[arr.length-1] = -1;
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
         return arr;
         
