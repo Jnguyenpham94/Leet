e5915fae64e4a5171b2dfef5b25575de6810bd69
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-21 22:52:59 -0700

    MoveZeroes FINISHED

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,17 +1,27 @@
 public class MoveZeroes {
-    
+
     /**
      * 
-     * @param nums array of ints
-     * nums:      0,1,0,3,12
+     * @param nums array of ints 
+     * nums: 0,1,0,3,12 
      * expected : 1,3,12,0,0
      */
     public void moveZeroes(int[] nums) {
-        for (int i : nums) {
-            if(i == 0){
-                
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int lastSeenZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastSeenZero++] = nums[i];
             }
         }
-        
+        for(int j = lastSeenZero; j < nums.length;j++){
+            nums[j] = 0;
+        }
+
+        //PrintStuff pr = new PrintStuff();
+        //pr.printArray(nums);
+
     }
 }
