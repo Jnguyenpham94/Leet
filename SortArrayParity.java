dab4cbeadad54679a89f1981db73f8bb59e5441b
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-21 23:07:46 -0700

    in progress need to reload values into 1 array

--- SortArrayParity.java
+++ SortArrayParity.java
@@ -1,5 +1,22 @@
 public class SortArrayParity {
+    /**
+     * 
+     * @param nums array of numbers
+     * @return array with all evens and then odds from nums
+     */
     public int[] sortArrayByParity(int[] nums) {
+        int [] evens = new int[nums.length]; //contains all even values 
+        int [] odds = new int[nums.length]; //contains all odd values
+        int ev = 0;
+        int od = 0;
+        for (int i : nums) {
+            if(i % 2 == 0){
+                evens[ev++] = i;
+            }
+            else{
+                odds[od++] = i;
+            }
+        }
         return nums;
     }
 }
