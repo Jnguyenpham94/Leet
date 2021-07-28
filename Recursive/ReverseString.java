23a598f2306e6f7f62f6a85761f8cc0e4791c123
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-28 16:56:31 -0700

    ReverseString FINISHED

--- Recursive/ReverseString.java
+++ Recursive/ReverseString.java
@@ -1,13 +1,24 @@
 package Recursive;
+
 public class ReverseString {
-    
+
+    public void helper(char[] s, int left, int right) {
+        if (left >= right)
+            return;
+        char tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+        helper(s, left, right);
+    }
+
     /**
      * recursively done
-     * @param s a string to be outputted in reverse
-     * s = h e l l o
+     * 
+     * @param s a string to be outputted in reverse 
+     * s = h e l l o 
      * s = o l l e h
      */
     public void reverseString(char[] s) {
-
+        helper(s, 0, s.length - 1);
     }
 }
