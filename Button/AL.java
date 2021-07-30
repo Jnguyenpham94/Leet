7641ccf6a2c4e6f266b1b28396b8ee1235624eca
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2021-07-29 18:35:52 -0700

    trying buttons

---
+++ Button/AL.java
@@ -0,0 +1,9 @@
+package Button;
+
+import java.awt.*;
+
+    public class AL extends Frame implements WindowListener,ActionListener {
+        TextField text = new TextField(20);
+        Button b;
+        private int numClicks = 0;
+    }
